3ce7d9b13a243b633345a6d708dfb65f5dd60bc9
ShizuR <raishizu@hotmail.com>
2026-06-16 17:38:25 +0100

    changed description of each button

--- siteContent/indexCode.js
+++ siteContent/indexCode.js
@@ -1,7 +1,9 @@
-function helloNeighbour(response) {
-    console.log("hello, i'm your neighbour")
-    return response
+function desc(type) {
+    switch(type) {
+        case 'second.html':
+            document.getElementById('desc').innerHTML = 'An online art application for vector based drawings. users are able to: change colour, erase vectors, and change brush size.'
+            break;
+        default:
+            document.getElementById('desc').innerHTML = ''
+    }
 }
-
-let b = helloNeighbour("goodbye, i'll see you later")
-helloNeighbour()
