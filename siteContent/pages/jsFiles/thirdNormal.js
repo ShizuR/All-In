795d7575c29404b28068494eacf0660c28b765cc
ShizuR <raishizu@hotmail.com>
2026-08-20 19:59:20 +0100

    disabled and enabled hover effects

--- siteContent/pages/jsFiles/thirdNormal.js
+++ siteContent/pages/jsFiles/thirdNormal.js
@@ -2,34 +2,44 @@
 // used to customize the map
 let currentPick = 'home';
 const allChoices = ['home', 'alterC', 'alterP']
+const allCountries = ['wales', 'northernI', 'scotland']
 
 // since the svg separates england into smaller areas, group them into the england class and treat them as one entity
 document.querySelectorAll('.england').forEach(function(e) {
     // hovering over england regions
-    e.addEventListener('mouseover', m => {
+    e.addEventListener('mouseover', e => {
         // change header
-        changeHeader('England');
-        document.querySelectorAll('.england').forEach(function(f){
-            f.setAttribute('style', 'fill: rgb(69, 162, 205)');
-        })
+        if (e.target.classList.contains(e.target.id + 'Hover')) {
+            changeHeader('England');
+            document.querySelectorAll('.england').forEach(function(f){
+                f.setAttribute('style', 'fill: rgb(69, 162, 205)');
+            })
+        }
     })
 
-    e.addEventListener('mouseout', m => {
-        changeHeader('Prisoners');
-        document.querySelectorAll('.england').forEach(function(f){
-            f.setAttribute('style', 'fill: rgb(100, 191, 233)');
-        })
+    e.addEventListener('mouseout', e => {
+        if (e.target.classList.contains(e.target.id + 'Hover')) {
+            changeHeader('Prisoners');
+            document.querySelectorAll('.england').forEach(function(f){
+                f.setAttribute('style', 'fill: rgb(100, 191, 233)');
+            })
+        }
     })
+    
 });
 
 // change header on hover
 document.querySelectorAll('.country').forEach(function(e) {
     e.addEventListener('mouseover', e => {
-        changeHeader(document.getElementById(e.target.id).getAttribute('title'));
+        if (e.target.classList.contains(e.target.id + 'Hover')) {
+            changeHeader(document.getElementById(e.target.id).getAttribute('title'));
+        }
     })
 
     e.addEventListener('mouseout', e => {
-        changeHeader('Prisoners');
+        if (e.target.classList.contains(e.target.id + 'Hover')) {
+            changeHeader('Prisoners');
+        }
     })
 });
 
@@ -39,12 +49,37 @@ function changeHeader(country) {
 
 function choiceClicked(name) {
     currentPick = name;
+    if (currentPick != 'home') {
+        allCountries.forEach(e => {
+            if (document.getElementById(e).classList.contains(e + 'Hover') == true) {
+                document.getElementById(e).classList.remove(e + 'Hover')
+            }
+        })
+        document.querySelectorAll('.england').forEach(function(e) {
+            if (e.classList.contains('englandHover') == true) {
+                e.classList.remove('englandHover')
+            }
+        })
+    }
+    else {
+        allCountries.forEach(e => {
+            if (document.getElementById(e).classList.contains(e + 'Hover') == false) {
+                document.getElementById(e).classList.add(e + 'Hover')
+            }
+        })
+        document.querySelectorAll('.england').forEach(function(e) {
+            if (e.classList.contains('englandHover') == false) {
+                e.classList.add('englandHover')
+            }
+        })
+    }
     allChoices.forEach(e => {
         document.getElementById(e).removeAttribute('disabled');
     });
     document.getElementById(name).setAttribute('disabled', 'True');
     if (name == 'home') {
         document.getElementById('dataHeader').innerText = 'Prisoners';
+        
     }
     else {
         document.getElementById('dataHeader').innerText = document.getElementById(name).innerText;
